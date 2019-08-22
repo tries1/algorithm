0386e563fcae4fde6eb9482267e9110372fbf55c
glenn <glenn.jo@kakaocorp.com>
2019-08-22 10:45:17 +0900

    가장큰수 추가

--- src/programmers/MaxNumber.java
+++ src/programmers/MaxNumber.java
@@ -2,10 +2,10 @@ package programmers;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- *
  * 문제 설명
  * 0 또는 양의 정수가 주어졌을 때, 정수를 이어 붙여 만들 수 있는 가장 큰 수를 알아내 주세요.
  * <p>
@@ -13,12 +13,12 @@ import java.util.stream.Stream;
  * <p>
  * 0 또는 양의 정수가 담긴 배열 numbers가 매개변수로 주어질 때, 순서를 재배치하여 만들 수 있는 가장 큰 수를 문자열로 바꾸어 return 하도록 solution 함수를 작성해주세요.
  * <p>
- *
+ * <p>
  * 제한 사항
  * numbers의 길이는 1 이상 100,000 이하입니다.
  * numbers의 원소는 0 이상 1,000 이하입니다.
  * 정답이 너무 클 수 있으니 문자열로 바꾸어 return 합니다.
- *
+ * <p>
  * 입출력 예
  * numbers	return
  * [6, 10, 2]	6210
@@ -31,48 +31,16 @@ public class MaxNumber {
         //9534330
         System.out.println(solution(numbers));
 
-
-        System.out.println("-------------------");
-
-
-
-
-        int n = numbers.length;
-        int[] combArr = new int[n];
-        doCombination(combArr, n, n, 0, 0, numbers);
     }
 
     public static String solution(int[] numbers) {
-
-        //int[] numbers1 = Arrays.stream(numbers).filter(i -> i < 10).boxed().sorted(Comparator.reverseOrder()).mapToInt(Integer::intValue).toArray();
-        //int[] numbers2 = Arrays.stream(numbers).filter(i -> i >= 10).boxed().sorted(Comparator.reverseOrder()).mapToInt(Integer::intValue).toArray();
-        //int[] concatNumbers = Stream.concat(Arrays.stream(numbers1).boxed(), Arrays.stream(numbers2).boxed()).mapToInt(Integer::intValue).toArray();
-        int[] sortedNumbers = Arrays.stream(numbers).boxed().sorted(Comparator.reverseOrder()).mapToInt(Integer::intValue).toArray();
-
-        String sum = "";
-        for (int i = 9; i >= 1; i--) {
-            for (int number : sortedNumbers) {
-                int checkNum = Integer.parseInt(String.valueOf(number).substring(0, 1));
-
-                if(i == checkNum){
-                    sum += String.valueOf(number);
-                }
-            }
-        }
-
-        return sum;
-    }
-
-
-    public static void doCombination(int[] combArr, int n, int r, int index, int target, int[] arr){
-        if(r == 0){
-            for(int i=0; i<index; i++) System.out.print(arr[combArr[i]] + " ");
-            System.out.println();
-        }else if(target == n) return;
-        else{
-            combArr[index] = target;
-            doCombination(combArr, n, r-1, index+1, target+1, arr); // (i)
-            doCombination(combArr, n, r, index, target+1, arr); //(ii)
-        }
+        return Arrays.stream(numbers)
+                .boxed()
+                .map(String::valueOf)
+                .sorted((o1, o2) -> {
+                    System.out.println(String.format("o2+o1 = %s, o1+o2 = %s", o2 + o1, o1 + o2));
+                    return (o2 + o1).compareTo(o1 + o2);
+                })
+                .collect(Collectors.joining());
     }
 }
