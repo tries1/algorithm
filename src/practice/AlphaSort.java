bf8ee451c9ca79618be4ead4edc293f36d272db2
glenn <glenn.jo@kakaocorp.com>
2019-08-23 14:31:46 +0900

    알파벳 대문자 정렬정리

--- src/practice/AlphaSort.java
+++ src/practice/AlphaSort.java
@@ -2,9 +2,12 @@ package practice;
 
 import java.util.Arrays;
 
+/**
+ * 문자열 비교시 compareTo는 사전적순서로 비교한다.
+ */
 public class AlphaSort {
     public static void main(String[] args) {
-        String alpha = "kashdfuihfFISVSDFKFCaaaWOYWEFIHaVBWIAFAOIAaXCmzxnvdfahgtyuikambnmkoiDMLaKJNdfsfhuaiwhVUEHwetvavxIVMXASCDAPUCDPAJ";
+        String alpha = "kashdfuihfFISVSDFKFCaaaWOYWEFIHaVBWIAFAOIAaXCmzxnGgvdfahgtyuikambnmkoiDMLaKJggNdfsfhuaiwhVUGEHwetvavxIVMXASCDAPUCDPAJ";
 
         System.out.println("case 1 :");
         Arrays.stream(alpha.split(""))
@@ -16,9 +19,17 @@ public class AlphaSort {
         System.out.println("case 2 :");
         Arrays.stream(alpha.split(""))
                 .sorted((o1, o2) -> {
-                    System.out.println(String.format("%s,%s = %s", o1, o2, o1.compareTo(o2)));
-                    int res = String.CASE_INSENSITIVE_ORDER.compare(o1, o2);
+                    int res = o1.compareToIgnoreCase(o2);
                     return (res == 0) ? o1.compareTo(o2) : res;
+                })
+                .forEach(System.out::print);
+
+        System.out.println();
+        System.out.println("case 3 :");
+        Arrays.stream(alpha.split(""))
+                .sorted((o1, o2) -> {
+                    int res = o1.compareToIgnoreCase(o2);
+                    return (res == 0) ? o2.compareTo(o1) : res;
                 })
                 .forEach(System.out::print);
 
