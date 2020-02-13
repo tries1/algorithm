f82559e71786650b06d219dead1d8db172d08974
glenn <glenn.jo@kakaocorp.com>
2020-02-13 15:59:33 +0900

    문제풀이

--- src/programmers/Tower.java
+++ src/programmers/Tower.java
@@ -1,12 +1,21 @@
 package programmers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
+import java.util.stream.IntStream;
 
 /**
  * 문제 설명
  * 수평 직선에 탑 N대를 세웠습니다. 모든 탑의 꼭대기에는 신호를 송/수신하는 장치를 설치했습니다. 발사한 신호는 신호를 보낸 탑보다 높은 탑에서만 수신합니다. 또한, 한 번 수신된 신호는 다른 탑으로 송신되지 않습니다.
  * <p>
- * 예를 들어 높이가 6, 9, 5, 7, 4인 다섯 탑이 왼쪽으로 동시에 레이저 신호를 발사합니다. 그러면, 탑은 다음과 같이 신호를 주고받습니다. 높이가 4인 다섯 번째 탑에서 발사한 신호는 높이가 7인 네 번째 탑이 수신하고, 높이가 7인 네 번째 탑의 신호는 높이가 9인 두 번째 탑이, 높이가 5인 세 번째 탑의 신호도 높이가 9인 두 번째 탑이 수신합니다. 높이가 9인 두 번째 탑과 높이가 6인 첫 번째 탑이 보낸 레이저 신호는 어떤 탑에서도 수신할 수 없습니다.
+ * 예를 들어 높이가 6, 9, 5, 7, 4인 다섯 탑이 왼쪽으로 동시에 레이저 신호를 발사합니다.
+ * 그러면, 탑은 다음과 같이 신호를 주고받습니다.
+ * 높이가 4인 다섯 번째 탑에서 발사한 신호는 높이가 7인 네 번째 탑이 수신하고
+ * 높이가 7인 네 번째 탑의 신호는 높이가 9인 두 번째 탑이
+ * 높이가 5인 세 번째 탑의 신호도 높이가 9인 두 번째 탑이 수신합니다.
+ * 높이가 9인 두 번째 탑과 높이가 6인 첫 번째 탑이 보낸 레이저 신호는 어떤 탑에서도 수신할 수 없습니다.
  * <p>
  * 송신 탑(높이)	수신 탑(높이)
  * 5(4)	4(7)
@@ -44,9 +53,16 @@ import java.util.Stack;
  */
 public class Tower {
     public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        IntStream.rangeClosed(1, 10)
+                .forEach(i -> stack.push(i));
+        //System.out.println(stack.pop());
 
-        int[] heights = {6, 9, 5, 7, 4};
-        solution(heights);
+
+        //int[] heights = {6, 9, 5, 7, 4};
+        //int[] heights = {3,9,9,3,5,7,2};
+        int[] heights = {1,5,3,6,7,6,5};
+        Arrays.stream(solution(heights)).forEach(System.out::println);
     }
 
     /**
@@ -64,27 +80,32 @@ public class Tower {
 
         int transmitTower = 0;
         int receiveTower = 0;
-        boolean isStack = false;
+        boolean isStack;
         Stack<Integer> stack = new Stack<>();
         for (int i = heights.length - 1; i >= 0; i--) {
+            isStack = false;
             transmitTower = heights[i];
 
             for (int j = i - 1; j >= 0; j--) {
                 receiveTower = heights[j];
                 if (transmitTower < receiveTower) {
-                    isStack = true;
                     stack.push(j + 1);
+                    isStack = true;
                     break;
-                } else {
-                    stack.push(0);
-
                 }
+            }
 
+            if (!isStack) {
+                stack.push(0);
             }
         }
 
-        stack.forEach(System.out::println);
-        return answer;
+        int[] result = new int[stack.size()];
+        int index = 0;
+        while (!stack.empty()) {
+            result[index++] = stack.pop();
+        }
+        return result;
     }
 
 
