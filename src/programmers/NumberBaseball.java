5089c27f4e3cf7ba4a2793c9f6c207ba797055af
glenn <glenn.jo@kakaocorp.com>
2020-02-21 14:57:23 +0900

    풀이중

--- src/programmers/NumberBaseball.java
+++ src/programmers/NumberBaseball.java
@@ -1,5 +1,11 @@
 package programmers;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+import java.util.function.Function;
+
 /**
  * 문제 설명
  * 숫자 야구 게임이란 2명이 서로가 생각한 숫자를 맞추는 게임입니다. 게임해보기
@@ -15,9 +21,9 @@ package programmers;
  * A : 123
  * B : 1스트라이크 1볼.
  * A : 356
- * B : 1스트라이크 0볼.
+ * B : 1스트라이크 0볼. <-- 1스트라이크이면, 첫번쨰 질문에서 같은 위치의 숫자가 동일한게 있는지 확인, 볼이면 같은 숫자가 있는지 확인
  * A : 327
- * B : 2스트라이크 0볼.
+ * B : 2스트라이크 0볼. <-- 2스트라이크면 숫자 2개와 위치가 맞다는거니, 이전 결과와 비교해서 첫번째에서 볼이있고,
  * A : 489
  * B : 0스트라이크 1볼.
  * 이때 가능한 답은 324와 328 두 가지입니다.
@@ -29,17 +35,65 @@ package programmers;
  * baseball의 각 행은 [세 자리의 수, 스트라이크의 수, 볼의 수] 를 담고 있습니다.
  * 입출력 예
  * baseball	return
- * [[123, 1, 1], [356, 1, 0], [327, 2, 0], [489, 0, 1]]	2
+ * [[123, 1, 1]
+ * ,[356, 1, 0]
+ * ,[327, 2, 0]
+ * ,[489, 0, 1]]	2
  * 입출력 예 설명
  * 문제에 나온 예와 같습니다.
  */
 public class NumberBaseball {
     public static void main(String[] args) {
-
+        int[][] baseball = {{123, 1, 1}, {356, 1, 0}, {327, 2, 0}, {489, 0, 1}};
+        System.out.println(solution(baseball));
     }
 
+    //완전탐색이니깐, 1 ~ 999까지 반복하면서 위조건에 동일한 갯수를 구해보자
     public static int solution(int[][] baseball) {
         int answer = 0;
-        return answer;
+
+        String answerNum;
+        Stack<String> cadinate = new Stack<>();
+        for (int i = 1; i <= 999; i++) {
+            answerNum = String.valueOf(i);
+            if ((answerNum.length() == 1 || answerNum.length() == 2) || Arrays.stream(answerNum.split("")).distinct().count() != 3) {
+                continue;
+            }
+
+            for (int[] ints : baseball) {
+                int[] answerNumbers = Arrays.stream(answerNum.split("")).map(Integer::parseInt).mapToInt(value -> value).toArray();
+                int[] numbers = Arrays.stream(String.valueOf(ints[0]).split("")).map(Integer::parseInt).mapToInt(value -> value).toArray();
+                int strikeCount = ints[1];
+                int ballCount = ints[2];
+
+                if(check(answerNumbers, numbers, strikeCount, ballCount)){
+                    cadinate.push(answerNum);
+                }
+            }
+
+        }
+
+
+        return cadinate.size();
+    }
+
+    public static boolean check(int[] answerNum, int[] guessNum, int strike, int ball) {
+        int tmpStrike = 0, tmpBall = 0;
+
+        for (int i = 0; i < 3; i++) {
+            //strike check
+            if (answerNum[i] == guessNum[i]) {
+                tmpStrike++;
+                continue;
+            }
+
+            for (int j = i; j < guessNum.length; j++) {
+                if (answerNum[j] == guessNum[j]) {
+                    tmpBall++;
+                }
+            }
+        }
+
+        return strike == tmpStrike && ball == (tmpStrike - tmpBall);
     }
 }
